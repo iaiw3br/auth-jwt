ba3ef2740946b1812ea466dd779081346504ddfe
Артем Иванов <iaiw3br@gmail.com>
2021-10-25 23:10:13 +0300

    feat: Добавлен файл с текстами ошибок

---
+++ internal/error/error.go
@@ -0,0 +1,10 @@
+package authError
+
+var (
+	ErrorInvalidJson      = "invalid Json"
+	ErrorInvalidLoginData = "invalid login data"
+	ErrorInvalidToken     = "invalid token"
+	ErrFailedStartServer  = "failed with start server"
+	ErrNotEvenAToken      = "that's not even a token"
+	ErrTokenIsExpired     = "token is expired"
+)
