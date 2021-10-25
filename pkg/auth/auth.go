2c03ce2e7f4af5df76663b1cef243d5222b88805
Артем Иванов <iaiw3br@gmail.com>
2021-10-25 23:10:44 +0300

    feat: Добавлены функции логирования и разлогирования

---
+++ pkg/auth/auth.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"main/internal/auth"
+	"main/internal/service"
+	"net/http"
+)
+
+func Login(c *gin.Context) {
+
+	err := service.CheckValidateUser(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, err)
+		return
+	}
+
+	username := viper.GetString("USERNAME")
+	token, err := auth.CreateToken(username)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, err)
+		return
+	}
+
+	c.SetCookie("access_token", token, 1500, "/", "localhost", false, true)
+	c.JSON(http.StatusOK, "success login")
+}
+
+func Logout(c *gin.Context) {
+	c.SetCookie("access", "", -1, "/", "localhost", false, true)
+	c.JSON(http.StatusOK, "success logout")
+}
