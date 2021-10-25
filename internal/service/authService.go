f14da05dbca8fe9fabafb6ed108cd55dcbbfffa0
Артем Иванов <iaiw3br@gmail.com>
2021-10-25 23:14:33 +0300

    feat: Добавлены функции для проверки пользовательских данных

---
+++ internal/service/authService.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+	authError "main/internal/error"
+	"main/internal/model"
+)
+
+func CheckValidateUser(c *gin.Context) error {
+	var user model.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		return errors.New(authError.ErrorInvalidJson)
+	}
+
+	username := viper.GetString("USERNAME")
+	password := viper.GetString("PASSWORD")
+
+	if username != user.Username || password != user.Password {
+		return errors.New(authError.ErrorInvalidLoginData)
+	}
+
+	return nil
+}
