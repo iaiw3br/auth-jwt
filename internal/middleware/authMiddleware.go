87f3a00678cbe9adfc946b605759239fe9406b50
Артем Иванов <iaiw3br@gmail.com>
2021-10-26 21:57:30 +0300

    feat: Добавление флага, если необходимо обновить токены

--- internal/middleware/authMiddleware.go
+++ internal/middleware/authMiddleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	authError "main/internal/error"
 	"main/internal/token"
 	"net/http"
 )
@@ -13,11 +14,26 @@ func TokenAuthMiddleware(c *gin.Context) {
 		c.Abort()
 	}
 
-	_, err = token.ValidateToken(accessToken)
+	refreshToken, err := c.Cookie("refresh_token")
 	if err != nil {
 		c.JSON(http.StatusUnauthorized, err.Error())
 		c.Abort()
 	}
+
+	isNeedUpdateTokens := token.CheckUpdateTokens(accessToken, refreshToken)
+
+	if isNeedUpdateTokens {
+		c.Set("refreshTokens", true)
+	}
+
+	username, err := token.GetUsernameFromToken(refreshToken, "SECRET_REFRESH")
+
+	if err != nil || username == "" {
+		c.JSON(http.StatusUnauthorized, authError.ErrorInvalidToken)
+		c.Abort()
+	}
+
+	c.Set("username", username)
 
 	c.Next()
 }
