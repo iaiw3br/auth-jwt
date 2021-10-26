465fe79eed07785e1351699289da525622ef3ec2
Артем Иванов <iaiw3br@gmail.com>
2021-10-26 21:57:46 +0300

    feat: Создание нескольких токенов и запись в куки

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -17,17 +17,20 @@ func Login(c *gin.Context) {
 	}
 
 	username := viper.GetString("USERNAME")
-	accessToken, err := token.CreateToken(username)
+	tokens, err := token.CreateTokens(username)
 	if err != nil {
 		c.JSON(http.StatusUnauthorized, err)
 		return
 	}
 
-	c.SetCookie("access_token", accessToken, 1500, "/", "localhost", false, true)
+	c.SetCookie("access_token", tokens["accessToken"], 60, "/", "localhost", false, true)
+	c.SetCookie("refresh_token", tokens["refreshToken"], 3600, "/", "localhost", false, true)
+
 	c.JSON(http.StatusOK, "success login")
 }
 
 func Logout(c *gin.Context) {
-	c.SetCookie("access", "", -1, "/", "localhost", false, true)
+	c.SetCookie("access_token", "", -1, "/", "localhost", false, true)
+	c.SetCookie("refresh_token", "", -1, "/", "localhost", false, true)
 	c.JSON(http.StatusOK, "success logout")
 }
