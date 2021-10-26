b177cf411c8b699df853a13501d70d31a532f41a
Артем Иванов <iaiw3br@gmail.com>
2021-10-26 21:56:32 +0300

    feat: Обновление токенов, если передан флаг refreshTokens

--- internal/user/user.go
+++ internal/user/user.go
@@ -2,10 +2,29 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+	authError "main/internal/error"
+	"main/internal/token"
 	"net/http"
 )
 
 func GetUserInformation(c *gin.Context) {
-	// TODO: Добавить информацию о пользователе
-	c.JSON(http.StatusOK, "There are must be full personal information.")
+	username, _ := c.Get("username")
+	if usernameString, ok := username.(string); ok {
+		_, ok := c.Get("refreshTokens")
+		if ok {
+			tokens, err := token.CreateTokens(usernameString)
+			if err != nil {
+				c.JSON(http.StatusUnauthorized, authError.ErrorInvalidToken)
+			}
+			c.SetCookie("access_token", tokens["accessToken"], 60, "/", "localhost", false, true)
+			c.SetCookie("refresh_token", tokens["refreshToken"], 3600, "/", "localhost", false, true)
+
+			c.JSON(http.StatusOK, "tokens are success refreshed")
+			return
+		}
+		c.JSON(http.StatusOK, username)
+		return
+	}
+
+	c.JSON(http.StatusUnauthorized, authError.ErrIncomingData)
 }
